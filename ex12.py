d762674d2ecf7ce49c1f241b4e052617d5fe5a83
zhou18520786640 <zhou18520786640@gmail.com>
2016-02-04 00:22:15 +0800

    ex12

--- ex12.py
+++ ex12.py
@@ -1,4 +1,9 @@
+#coding:utf-8
+
 age = raw_input("How old are you?")
 height = raw_input("How tall are you?")
 weight = raw_input("How much do you weight?")
 print "so, you're %r old, %r tall and %r heavy." % (age, height, weight)
+
+# pydoc 作用
+#

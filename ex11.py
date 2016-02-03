5781ffec3003e82a419cb5d805206ac1b3386ee6
zhou18520786640 <zhou18520786640@gmail.com>
2016-02-04 00:07:58 +0800

    ex11 加分题的注释

--- ex11.py
+++ ex11.py
@@ -1,8 +1,12 @@
+#coding:utf-8
+
 print "How old are you?",
 age = raw_input()
-print "How tall are you?"
+print "How tall are you?",
 height = raw_input()
 print "How much do you weigh?",
 weight = raw_input()
 
 print "So, you're %r old, %r tall and %r heavy." % (age, height, weight)
+
+# raw_input则是不管用户输入什么类型的都会转变成字符型.

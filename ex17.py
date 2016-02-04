e95412b2451bbbe1d621c2330af39c30cf755342
zhou18520786640 <zhou18520786640@gmail.com>
2016-02-04 23:55:39 +0800

    ex17

---
+++ ex17.py
@@ -0,0 +1,21 @@
+from sys import argv
+from os.path import exists
+
+script, from_file, to_file = argv
+
+print "Copying from %s to %s" % (from_file, to_file)
+
+# we colud do these two on one line too, how ? 
+input = open(from_file)
+indata = input.read()
+
+print "The input file is %d bytes long" % len(indata)
+
+print "Does the output file exist?%r" % exists(to_file)
+raw_input()
+output = open(to_file, 'w')
+output.write(indata)
+
+print "Alright,all done"
+output.close()
+input.close()

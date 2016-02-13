f8c7b82c73d86dddc2f7922fc58c34125dfdac9c
zhou18520786640 <zhou18520786640@gmail.com>
2016-02-13 11:23:21 +0800

    ex46

---
+++ gothonweb/bin/setup.py
@@ -0,0 +1,19 @@
+try:
+	from setuptools	import setup
+except ImportError:
+	from distutils.core import setup
+	
+config = {
+	'description':'ex46'
+	'author':'zhou'
+	'url':'URL to get it at'
+	'download_url':'Where to download it'
+	'author_email':'mmix1009@163.com'
+	'version':'0.1',
+	'install_requires':['iOS']	
+	'packages':['skeleton'],
+	'scripts':[],
+	'name':'projectname'
+}
+
+setup(**config)
---
+++ projects/skeleton/tests/skeleton_test.py
@@ -0,0 +1,12 @@
+from nose.tools import *
+import skeleton
+
+def setup():
+	print "STETUP!"
+
+def teardown():
+	print "TEAR DOWN!"
+
+def test_basic():
+	print "I RAN!"
+	
